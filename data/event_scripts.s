a13f822e369d793a3fb3933e62e524e73e15397f
Liamjd14 <Liamjdgaming@gmail.com>
2024-12-15 18:07:43 +0000

    Shadewind Wilds Map and Other Map Updates

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1175,3 +1175,5 @@ EventScript_VsSeekerChargingDone::
 	.include "data/maps/Shadewind/scripts.inc"
 
 	.include "data/maps/ShadewindDawncoralMapFill/scripts.inc"
+
+	.include "data/maps/ShadewindWilds/scripts.inc"
