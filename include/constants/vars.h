14750ef587b741a9e0991ff9037b7f67baae6b21
Liamjd14 <Liamjdgaming@gmail.com>
2025-01-20 12:36:14 +0000

    Expansion Config Setup
    Changes all config to match planned features and assignes flags and vars when necicary

--- include/constants/vars.h
+++ include/constants/vars.h
@@ -264,14 +264,14 @@
 #define VAR_ROXANNE_CALL_STEP_COUNTER                    0x40F4
 #define VAR_SCOTT_BF_CALL_STEP_COUNTER                   0x40F5
 #define VAR_RIVAL_RAYQUAZA_CALL_STEP_COUNTER             0x40F6
-#define VAR_UNUSED_0x40F7                                0x40F7 // Unused Var
-#define VAR_UNUSED_0x40F8                                0x40F8 // Unused Var
-#define VAR_UNUSED_0x40F9                                0x40F9 // Unused Var
-#define VAR_UNUSED_0x40FA                                0x40FA // Unused Var
-#define VAR_UNUSED_0x40FB                                0x40FB // Unused Var
-#define VAR_UNUSED_0x40FC                                0x40FC // Unused Var
-#define VAR_UNUSED_0x40FD                                0x40FD // Unused Var
-#define VAR_UNUSED_0x40FE                                0x40FE // Unused Var
+#define VAR_SYS_BATTLE_STARTING_STATUS                                0x40F7 // Unused Var
+#define VAR_SYS_BATTLE_STARTING_STATUS_TIMER                                0x40F8 // Unused Var
+#define VAR_SYS_BATTLE_WILD_AI_FLAGS                                0x40F9 // Unused Var
+#define VAR_SYS_BATTLE_DIFFICULTY                                0x40FA // Unused Var
+#define VAR_SYS_BATTLE_SKY_BATTLE                                0x40FB // Unused Var
+#define VAR_SYS_CAPS_LEVEL_CAP                             0x40FC // Unused Var
+#define VAR_SYS_DN_SPECIES                                0x40FD // Unused Var
+#define VAR_SYS_DN_STEP_COUNTER                               0x40FE // Unused Var
 #define VAR_UNUSED_0x40FF                                0x40FF // Unused Var
 
 #define VARS_END                                         0x40FF
