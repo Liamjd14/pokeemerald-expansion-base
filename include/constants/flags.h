fae52fe8a8310f27b38376723eeebafbf9db8229
Liam <Liamjdgaming@gmail.com>
2026-09-11 17:59:54 +0100

    Respawn Hidden items

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -753,6 +753,8 @@
 #define FLAG_UNUSED_0x2BA  0x2BA // Unused Flag
 #define FLAG_UNUSED_0x2BB  0x2BB // Unused Flag
 
+#define FLAG_HIDDEN_ITEMS_END FLAG_UNUSED_0x2BB
+
 // Event Flags
 #define FLAG_HIDE_ROUTE_101_BIRCH_STARTERS_BAG                      0x2BC
 #define FLAG_HIDE_APPRENTICE                                        0x2BD
