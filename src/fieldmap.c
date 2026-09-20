790496c054332514248292e245fc4603b0e93643
Liam <Liamjdgaming@gmail.com>
2026-09-20 19:59:07 +0100

    Tileset compile issue fix

--- src/fieldmap.c
+++ src/fieldmap.c
@@ -478,34 +478,34 @@ u32 ExtractMetatileAttribute(u32 attributes, u8 attributeType, bool32 isFrlg)
     return (attributes & sMetatileAttrMasksEmerald[attributeType]) >> sMetatileAttrShiftsEmerald[attributeType];
 }
 
-static u32 GetAttributeByMetatileIdAndMapLayoutFrlg(u16 metatile, u8 attributeType)
-{
-    u32 attribute;
-    if (metatile < GetNumMetatilesInPrimary(gMapHeader.mapLayout))
-    {
-        const u16 *attributes = (const u16*)gMapHeader.mapLayout->primaryTileset->metatileAttributes;
-        attribute = attributes[metatile];
-    }
-    else if (metatile < NUM_METATILES_TOTAL)
-    {
-        const u16 *attributes = (const u16*)gMapHeader.mapLayout->secondaryTileset->metatileAttributes;
-        metatile -= GetNumMetatilesInPrimary(gMapHeader.mapLayout);
-        attribute = attributes[metatile];
-    }
-    else
-    {
-        return MB_INVALID;
-    }
+// static u32 GetAttributeByMetatileIdAndMapLayoutFrlg(u16 metatile, u8 attributeType)
+// {
+//     u32 attribute;
+//     if (metatile < GetNumMetatilesInPrimary(gMapHeader.mapLayout))
+//     {
+//         const u32 *attributes = (const u32*)gMapHeader.mapLayout->primaryTileset->metatileAttributes;
+//         attribute = attributes[metatile];
+//     }
+//     else if (metatile < NUM_METATILES_TOTAL)
+//     {
+//         const u32 *attributes = (const u32*)gMapHeader.mapLayout->secondaryTileset->metatileAttributes;
+//         metatile -= GetNumMetatilesInPrimary(gMapHeader.mapLayout);
+//         attribute = attributes[metatile];
+//     }
+//     else
+//     {
+//         return MB_INVALID;
+//     }
 
-    return ExtractMetatileAttribute(attribute, attributeType, FALSE);
-}
+//     return ExtractMetatileAttribute(attribute, attributeType, TRUE);
+// }
 
 u32 GetAttributeByMetatileIdAndMapLayout(u16 metatile, u8 attributeType, bool32 isFrlg)
 {
     u32 attribute;
 
-    if (isFrlg)
-        return GetAttributeByMetatileIdAndMapLayoutFrlg(metatile, attributeType);
+//    if (isFrlg)
+//        return GetAttributeByMetatileIdAndMapLayoutFrlg(metatile, attributeType);
 
     if (metatile < GetNumMetatilesInPrimary(gMapHeader.mapLayout))
     {
