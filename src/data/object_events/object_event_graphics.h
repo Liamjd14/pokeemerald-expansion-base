4898a69fe17e1b733dbd318af6c7cd9780d238fa
Liam <Liamjdgaming@gmail.com>
2026-09-21 18:19:39 +0100

    Taunie Sprite Test

--- src/data/object_events/object_event_graphics.h
+++ src/data/object_events/object_event_graphics.h
@@ -633,3 +633,7 @@ const u16 gObjectEventPic_PidgeyOld[] = INCGFX_U16("graphics/object_events/pics/
 const u16 gObjectEventPic_ClefairyOld[] = INCGFX_U16("graphics/object_events/pics/pokemon_old/clefairy.png", ".4bpp", "-mwidth 2 -mheight 2");
 
 #endif // IS_FRLG
+// custom
+
+const u16 gObjectEventPic_Taunie[] = INCGFX_U16("graphics/object_events/pics/people/custom/taunie.png", ".4bpp", "-mwidth 2 -mheight 4");
+const u16 gObjectEventPal_Taunie[] = INCGFX_U16("graphics/object_events/palettes/taunie.pal", ".gbapal");
